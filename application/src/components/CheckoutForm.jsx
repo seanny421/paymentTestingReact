e1320a21ce5cf981a8e7ac0954e36b3021c0b484
rtb17137 <sean.mcgeachie.2017@uni.strath.ac.uk>
2021-11-09 19:23:28 +0000

    updated stuff

--- application/src/components/CheckoutForm.jsx
+++ application/src/components/CheckoutForm.jsx
@@ -3,19 +3,22 @@ import Item from "./Item.jsx";
 import {CardElement, CardElementContainer} from "@stripe/react-stripe-js";
 import {loadStripe} from "@stripe/stripe-js";
 
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider, Page, Card, Button, FormLayout, TextField} from "@shopify/polaris";
 
 export default function CheckoutForm(){
-const stripePromise = loadStripe("pk_test_qblFNYngBkEdjEZ16jxxoWSM");
     return (
         <div>
-            <form>
-                {/* <CardElementContainer> */}
-                    <CardElement/>
-                    
-                {/* </CardElementContainer> */}
-            </form>
-            <button>Submit</button>
+            <AppProvider i18n={enTranslations}>
+                <Page title="Example App">
+                    <Card sectioned>
+                        <FormLayout.Group>
+                            <TextField value="" label="firsname" placeholder="sean"/>
+                        <Button primary onClick={() => alert('Button clicked!')}>Example button</Button>
+                        </FormLayout.Group>
+                    </Card>
+                </Page>
+            </AppProvider>
         </div>
-
     );
 }
